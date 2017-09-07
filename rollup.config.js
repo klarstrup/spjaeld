489ba082b1e779261baf8565a3a57ca116313893
Jonas Klarstrup <jonas@klarstrup.dk>
2017-09-07 02:48:19 +0200

    add build shizzle

---
+++ rollup.config.js
@@ -0,0 +1,79 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
+export default [
+  {
+    input: 'src/index.js',
+    name: 'spjaeld',
+    output: {
+      file: 'dist/spjaeld.umd.js',
+      format: 'umd',
+    },
+    plugins: [
+      resolve(),
+      babel({
+        presets: [
+          [
+            'env',
+            {
+              targets: {
+                browsers: ['> 1%'],
+              },
+              modules: false,
+            },
+          ],
+        ],
+      }),
+    ],
+  },
+{
+    input: 'src/index.js',
+    name: 'spjaeld',
+    output: {
+      file: 'dist/spjaeld.umd.min.js',
+      format: 'umd',
+    },
+    plugins: [
+      resolve(),
+      babel({
+        presets: [
+          [
+            'env',
+            {
+              targets: {
+                browsers: ['> 1%'],
+              },
+              modules: false,
+            },
+          ],
+        ],
+      }),
+      uglify(),
+    ],
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/spjaeld.cjs.js',
+      format: 'cjs',
+    },
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**', // only transpile our source code
+        presets: [
+          [
+            'env',
+            {
+              targets: {
+                node: 'current',
+              },
+              modules: false,
+            },
+          ],
+        ],
+      }),
+    ],
+  },
+];
